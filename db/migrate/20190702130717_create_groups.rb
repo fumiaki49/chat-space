56899626db959752d8dc98bb4133556fd3600a51
fumiaki49 <me.fk39@gmail.com>
2019-07-03 12:10:34 +0900

    add name column to groups table

--- db/migrate/20190702130717_create_groups.rb
+++ db/migrate/20190702130717_create_groups.rb
@@ -1,8 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
-      
+      t.string :name, unll: false
+      t.index :name, unique: true
       t.timestamps
     end
   end
