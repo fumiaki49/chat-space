6ddd470f9ad61da156aacd1ae41121a1c2133f12
fumiaki49 <me.fk39@gmail.com>
2019-07-04 15:55:46 +0900

    create group-creating-system

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,9 +12,9 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-
+  
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, :user_ids => [] )
   end
 end
