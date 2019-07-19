c888c4155d5bd46fa1c2002f023048880ad2de0d
fumiaki49 <me.fk39@gmail.com>
2019-07-19 17:40:49 +0900

    create html message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,27 @@
 $(function() {
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id= "${message.id}">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">
+                    ${message.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                    ${message.date}
+                    </div>
+                  </div>
+                  <div class="message__text">
+                    <p class="lower-message__content">
+                    ${content}
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+    return html;
+  }
+
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -9,7 +32,15 @@ $(function() {
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".messages").append(html)
+      $("#message_content").val("")
+    })
+    .fail(function(){
+      alert('エラーが発生したため送信できませんでした。');
     })
   });
 });
