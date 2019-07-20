90a4faef13572d4ad7d159c7e525e50d4ba1c308
fumiaki49 <me.fk39@gmail.com>
2019-07-20 15:36:49 +0900

    change .val to .reset method message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -37,7 +37,7 @@ $(document).on('turbolinks:load', function() {
     .done(function(data){
       var html = buildHTML(data);
       $(".messages").append(html);
-      $("#message_content").val("");
+      $(".new_message")[0].reset();
       $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight});
     })
     .fail(function(){
