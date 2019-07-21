95cee7ea3d49f798aa82608275cd04e53a8424a2
fumiaki49 <me.fk39@gmail.com>
2019-07-21 12:00:55 +0900

    add index and search action users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.name
+  end
+
   def edit
   end
 
@@ -9,6 +13,14 @@ class UsersController < ApplicationController
     else
       render :edit
     end 
+  end
+
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json 
+    end
   end
 
   private
