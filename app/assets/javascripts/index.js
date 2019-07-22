bdb49536835bd8fd5231c7975b1513d21889167d
fumiaki49 <me.fk39@gmail.com>
2019-07-22 21:06:30 +0900

    create index.js and incremental-search

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,44 @@
+$(document).on('turbolinks:load', function() {
+
+  var searchResult = $('#user-search-result');
+
+  function appendbuildHTML(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
+                </div>`
+    searchResult.append(html);
+  }
+
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ msg }</p>
+                </div>`
+    searchResult.append(html);
+  }
+
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json',
+    })
+    .done(function(users) {
+      $('#user-search-result').empty();
+        if (users.length !== 0) {
+            users.forEach(function(user) {
+            appendbuildHTML(user);
+          });
+        }
+        else {
+          appendErrMsgToHTML("一致するユーザーがいません");
+        }
+      })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました。');
+    })
+  });
+});
