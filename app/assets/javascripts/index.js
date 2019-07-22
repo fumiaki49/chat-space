f7a36d906287b6df617e19f790dad79d7cc6e693
fumiaki49 <me.fk39@gmail.com>
2019-07-22 23:18:41 +0900

    create click event to delete search result index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,7 +5,7 @@ $(document).on('turbolinks:load', function() {
   function appendbuildHTML(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                 </div>`
     searchResult.append(html);
   }
@@ -41,4 +41,8 @@ $(document).on('turbolinks:load', function() {
       alert('ユーザー検索に失敗しました。');
     })
   });
+
+  $('#user-search-result').on("click", 'a', function() {
+    $("#user-search-result").empty();
+  })
 });
